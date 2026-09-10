c7f8e66ad835202c00b7d8327e4647beb3e367af
Mari <123911714+cryptoqueen23@users.noreply.github.com>
2026-09-10 14:15:29 -0500

    Add Gatesville route

---
+++ app/gatesville/page.tsx
@@ -0,0 +1,2 @@
+import CityHome from '@/components/CityHome';
+export default function Gatesville(){return <CityHome city="Gatesville" subtitle="Coryell County, Texas" officialUrl="https://www.gatesvilletx.com"/>}
