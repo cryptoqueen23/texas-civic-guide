dc8dbb919a366be277ea47a7b88e7167917950c7
Mari <123911714+cryptoqueen23@users.noreply.github.com>
2026-09-10 16:02:27 -0500

    Link Gatesville bilingual council guide

--- app/gatesville/page.tsx
+++ app/gatesville/page.tsx
@@ -1,2 +1,9 @@
 import CityHome from '@/components/CityHome';
-export default function Gatesville(){return <CityHome city="Gatesville" subtitle="Coryell County, Texas" officialUrl="https://www.gatesvilletx.com"/>}
+import CouncilGuideBanner from '@/components/CouncilGuideBanner';
+
+export default function Gatesville(){
+  return <>
+    <CouncilGuideBanner city="Gatesville" />
+    <CityHome city="Gatesville" subtitle="Coryell County, Texas" officialUrl="https://www.gatesvilletx.com" />
+  </>;
+}
