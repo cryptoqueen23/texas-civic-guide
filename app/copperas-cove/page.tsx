9223a1cbdcc1eb708e587c1f82112601231ecebf
Mari <123911714+cryptoqueen23@users.noreply.github.com>
2026-09-10 14:15:24 -0500

    Add Copperas Cove route

---
+++ app/copperas-cove/page.tsx
@@ -0,0 +1,2 @@
+import CityHome from '@/components/CityHome';
+export default function CopperasCove(){return <CityHome city="Copperas Cove" subtitle="Coryell County, Texas" officialUrl="https://www.copperascovetx.gov"/>}
